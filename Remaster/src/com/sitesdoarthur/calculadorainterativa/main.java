9fe20784b09a55d381ae42d536cc41443dd2655a
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-07-31 21:59:58 -0300

    soma e subtracao

--- Remaster/src/com/sitesdoarthur/calculadorainterativa/main.java
+++ Remaster/src/com/sitesdoarthur/calculadorainterativa/main.java
@@ -15,5 +15,24 @@ public class main {
         System.out.println("5 - Potenciação");
         System.out.println("6 - Raiz Quadrada");
         System.out.println("7 - Sair");
+        System.out.println("Diga sua opção: ");
+        byte opcao = teclado.nextByte();
+        System.out.print("Digite o primeiro número: ");
+        int n1 = teclado.nextInt();
+        System.out.print("Digite o segundo número: ");
+        int n2 = teclado.nextInt();
+        switch (opcao) {
+            case 1:
+                int soma = n1 + n2;
+                System.out.printf("A soma de %d + %d é: %d\n", n1, n2, soma);
+                break;
+            case 2:
+                int subtracao = n1 - n2;
+                System.out.printf("A subtração de %d - %d é: %d\n", n1, n2, subtracao);
+                break;
+                
+            default:
+                break;
+        }
     }
 }
