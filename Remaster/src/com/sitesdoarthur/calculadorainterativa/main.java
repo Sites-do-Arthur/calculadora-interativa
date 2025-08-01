84a5fda0d228cf70e4483331fdd797132311c840
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2025-07-31 21:53:58 -0300

    menu feito

---
+++ Remaster/src/com/sitesdoarthur/calculadorainterativa/main.java
@@ -0,0 +1,19 @@
+package com.sitesdoarthur.calculadorainterativa;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("********************************************************************************");
+        System.out.println("****************************CALCULADORA INTERATIVA******************************");
+        System.out.println("********************************************************************************");
+        System.out.println("****MENU****");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+        System.out.println("5 - Potenciação");
+        System.out.println("6 - Raiz Quadrada");
+        System.out.println("7 - Sair");
+    }
+}
